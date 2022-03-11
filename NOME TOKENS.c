dcc7573989cfb0756a9a4d47425377c5fe960198
Amanda <amandaveras1999@gmail.com>
2022-03-10 23:22:53 -0300

    Acrescentando código da prof

--- NOME TOKENS.c
+++ NOME TOKENS.c
@@ -26,142 +26,227 @@
 #define OR 276;
 #define NOT 277;
 
+struct Token{
+ int nome_token;
+ int atributo;
+};
 
 
-//subrotina relop
-case 0:
-    c = code[cont_sim_lido];
-    if((c == ' ')||(c == '\n'))
-    {
-        estado = 0;
-        cont_sim_lido++;
-    }
-    else if(c == '<') estado = 1;
-    else if(c == '=') estado = 5;
-    else if(c == '>') estado = 6;
-    else
-        {
-            estado = falhar();
+
+int estado = 0;
+int partida = 0;
+int cont_sim_lido = 0;
+char *code;
+
+
+char *readFile(char *fileName)
+{
+	FILE *file = fopen(fileName, "r");
+	char *code;
+	int n = 0;
+	int c;
+
+	if(file == NULL) return NULL;
+
+	fseek(file, 0, SEEK_END);
+	long f_size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	code = new char (f_size);
+
+	while ((c = fgetc(file))!= EOF)
+	{
+		code[n++] = (char) c;
+	}
+	code[n] = '\0';
+	return code;
+}
+
+
+
+
+int falhar()
+{
+	switch(estado)
+	{
+	case 0: partida = 9; break;
+
+	case 9: partida = 12; break;
+
+	case 12: partida = 20; break;
+
+	case 20: partida = 25; break;
+
+	case 25:
+		//retornar msg de erro
+		printf("Erro encontrado no código\n");
+		cont_sim_lido++;
+		break;
+
+	default: printf("Erro do compilador");
+	}
+	return(partida);
+}
+
+Token proximo_token()
+{
+	Token token;
+	char c;
+	while(code[cont_sim_lido] != EOF){
+		switch(estado){
+
+            //subrotina relop
+            case 0:
+                c = code[cont_sim_lido];
+                if((c == ' ')||(c == '\n'))
+                {
+                    estado = 0;
+                    cont_sim_lido++;
+                }
+                else if(c == '<') estado = 1;
+                else if(c == '=') estado = 5;
+                else if(c == '>') estado = 6;
+                else
+                    {
+                        estado = falhar();
+                    }
+                break;
+            case 1:
+                cont_sim_lido++;
+                c = code[cont_sim_lido];
+
+                if(c == '=') estado = 2;
+                else if(c == '>') estado = 3;
+                else estado = 4;
+                break;
+            case 2:
+                cont_sim_lido++;
+                printf("<relop, LE>\n");
+                token.nome_token = RELOP;
+                token.atributo = LE;
+                estado = 0;
+                return(token);
+                break;
+
+            case 3:
+                cont_sim_lido++;
+                printf("<relop, NE>\n");
+                token.nome_token = RELOP;
+                token.atributo = NE;
+                estado = 0;
+                return(token);
+                break;
+
+            case 4:
+                cont_sim_lido++;
+                printf("<relop, LT>\n");
+                token.nome_token = RELOP;
+                token.atributo = LT;
+                estado = 0;
+                return(token);
+                break;
+
+            case 5:
+                cont_sim_lido++;
+                printf("<relop, EQ>\n");
+                token.nome_token = RELOP;
+                token.atributo = EQ;
+                estado = 0;
+                return(token);
+                break;
+
+            case 6:
+                cont_sim_lido++;
+                c = code[cont_sim_lido];
+                if(c == '=') estado = 7;
+                else estado = 8;
+                break;
+
+            case 7:
+                cont_sim_lido++;
+                printf("<relop, GE>\n");
+                token.nome_token = RELOP;
+                token.atributo = GE;
+                estado = 0;
+                return(token);
+                break;
+
+            case 8:
+                cont_sim_lido++;
+                printf("<relop, GT>\n");
+                token.nome_token = RELOP;
+                token.atributo = GT;
+                //ATENÇÃO - CORREÇÃO: foi acrescentado o comando "estado = 0;"
+                estado = 0;
+                return(token);
+                break;
+            //fim subrotina relop
+
+            //inicio subrotina logop
+            case 9:
+                c = code[cont_sim_lido];
+                if((c == ' ')||(c == '\n'))
+                {
+                    estado = 0;
+                    cont_sim_lido++;
+                }
+                else if(c == 'and') estado = 10;
+                else if(c == 'or') estado = 11;
+                else if(c == 'not') estado = 12;
+                else
+                {
+                    estado = falhar();
+                }
+                break;
+            //fim subrotina logop
+
+            //inicio subrotina ariop
+            case 10:
+                c = code[cont_sim_lido];
+                if((c == ' ')||(c == '\n'))
+                {
+                    estado = 0;
+                    cont_sim_lido++;
+                }
+                else if(c == 'and') estado = 10;
+                else if(c == 'or') estado = 11;
+                else if(c == 'not') estado = 12;
+                else
+                {
+                    estado = falhar();
+                }
+                break;
+            //fim subrotina ariop
+            case 11:
+                c = code[cont_sim_lido];
+                if((c == ' ')||(c == '\n'))
+                {
+                    estado = 0;
+                    cont_sim_lido++;
+                }
+                else if(c == 'and') estado = 10;
+                else if(c == 'or') estado = 11;
+                else if(c == 'not') estado = 12;
+                else
+                {
+                    estado = falhar();
+                }
+                break;
         }
-    break;
-case 1:
-    cont_sim_lido++;
-    c = code[cont_sim_lido];
 
-    if(c == '=') estado = 2;
-    else if(c == '>') estado = 3;
-    else estado = 4;
-    break;
-case 2:
-    cont_sim_lido++;
-    printf("<relop, LE>\n");
-    token.nome_token = RELOP;
-    token.atributo = LE;
-    estado = 0;
-    return(token);
-    break;
+	}
+	token.nome_token = EOF;
+	token.atributo = -1;
+	return(token);
+}
 
-case 3:
-    cont_sim_lido++;
-    printf("<relop, NE>\n");
-    token.nome_token = RELOP;
-    token.atributo = NE;
-    estado = 0;
-    return(token);
-    break;
+int main ()
+{
+	Token token;
+    code = readFile("programa.txt");
+    token = proximo_token();
+    token = proximo_token();
+    //...
 
-case 4:
-    cont_sim_lido++;
-    printf("<relop, LT>\n");
-    token.nome_token = RELOP;
-    token.atributo = LT;
-    estado = 0;
-    return(token);
-    break;
 
-case 5:
-    cont_sim_lido++;
-    printf("<relop, EQ>\n");
-    token.nome_token = RELOP;
-    token.atributo = EQ;
-    estado = 0;
-    return(token);
-    break;
-
-case 6:
-    cont_sim_lido++;
-    c = code[cont_sim_lido];
-    if(c == '=') estado = 7;
-    else estado = 8;
-    break;
-
-case 7:
-    cont_sim_lido++;
-    printf("<relop, GE>\n");
-    token.nome_token = RELOP;
-    token.atributo = GE;
-    estado = 0;
-    return(token);
-    break;
-
-case 8:
-    cont_sim_lido++;
-    printf("<relop, GT>\n");
-    token.nome_token = RELOP;
-    token.atributo = GT;
-    //ATENÇÃO - CORREÇÃO: foi acrescentado o comando "estado = 0;"
-    estado = 0;
-    return(token);
-    break;
-//fim subrotina relop
-
-//inicio subrotina logop
-case 9:
-    c = code[cont_sim_lido];
-    if((c == ' ')||(c == '\n'))
-    {
-        estado = 0;
-        cont_sim_lido++;
-    }
-    else if(c == 'and') estado = 10;
-    else if(c == 'or') estado = 11;
-    else if(c == 'not') estado = 12;
-    else
-    {
-        estado = falhar();
-    }
-    break;
-//fim subrotina logop
-
-//inicio subrotina ariop
-case 10:
-    c = code[cont_sim_lido];
-    if((c == ' ')||(c == '\n'))
-    {
-        estado = 0;
-        cont_sim_lido++;
-    }
-    else if(c == 'and') estado = 10;
-    else if(c == 'or') estado = 11;
-    else if(c == 'not') estado = 12;
-    else
-    {
-        estado = falhar();
-    }
-    break;
-//fim subrotina ariop
-case 11:
-    c = code[cont_sim_lido];
-    if((c == ' ')||(c == '\n'))
-    {
-        estado = 0;
-        cont_sim_lido++;
-    }
-    else if(c == 'and') estado = 10;
-    else if(c == 'or') estado = 11;
-    else if(c == 'not') estado = 12;
-    else
-    {
-        estado = falhar();
-    }
-    break;
+}
